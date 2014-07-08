53b56da8086365338c8c9bdb8c248c945d227f3c
Nick Lau <nicklauftw@gmail>
2014-07-08 07:45:06 -0500

    Latest

--- app/objects.js
+++ app/objects.js
@@ -3,7 +3,12 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+      newA = {
+        name : obj.name,
+        greeting : obj.greeting,
+        newFn : fn
+      }
+      return newA.newFn();
     },
 
     alterObjects : function(constructor, greeting) {
